b97f7738fe56a007b45b42fdc5eaf6ad31bf8e35
amelnik92f <amelnik92f@gmail.com>
2020-10-14 02:18:28 +0300

    add router config

--- src/router.js
+++ src/router.js
@@ -3,50 +3,90 @@ const { mountForms } = require("./form");
 import { UserList } from "./users";
 import users from "./data.json";
 
-const LOGIN_ROUTE = "/";
-const MAIN_ROUTE = "/main";
+export const LOGIN_ROUTE = "/login";
+export const MAIN_ROUTE = "/";
+export const CLIENT_ROUTE = "/clients";
+export const NOT_FOUND = "/not-found";
 
 const routerConfig = {
-  [MAIN_ROUTE]: () => {
-    // protectRoute(true);
-    const userList = new UserList(users);
-    userList.appendMeTo(PAGE_REF);
-  },
+  [MAIN_ROUTE]: withPageWrapper(
+    withLoginProtected((user, page) => {
+      page.innerHTML = "";
+
+      const userInfoElement = document.createElement("div");
+      userInfoElement.innerHTML = `<div>${user.name} пользователь в здании.</div>`;
+
+      page.appendChild(userInfoElement);
+    })
+  ),
+
+  [CLIENT_ROUTE]: withPageWrapper(
+    withLoginProtected((user, page) => {
+      page.innerHTML = "";
+      const userList = new UserList(users);
+      userList.appendMeTo(page);
+    })
+  ),
+
   [LOGIN_ROUTE]: () => {
+    PAGE_REF.innerHTML = "";
     const form = mountForms();
     PAGE_REF.appendChild(form);
   },
-};
 
-const router = () => {
-  const trimmed = getTrimedRoute(window.location.hash);
-  console.log(trimmed);
-  if (routerConfig[trimmed]) {
-    routerConfig[trimmed]();
-  } else {
-    routerConfig[LOGIN_ROUTE];
-  }
-};
-
-export const initRouter = () => {
-  window.addEventListener("hashchange", (ev) => {
+  [NOT_FOUND]: () => {
     PAGE_REF.innerHTML = "";
-    router();
-  });
+    const notFound = document.createElement("div");
+    notFound.innerText = `Page is not found((`;
 
-  if (!window.location.hash) {
-    window.location.hash = LOGIN_ROUTE;
-  } else {
-    router();
-  }
+    PAGE_REF.appendChild(notFound);
+  },
 };
 
 const getTrimedRoute = (path) => {
   return path.replace(/^#/g, "");
 };
 
-export const protectRoute = (isForbidden) => {
-  if (isForbidden) {
-    window.location.hash = LOGIN_ROUTE;
+const router = () => {
+  const trimmed = window.location.hash
+    ? getTrimedRoute(window.location.hash)
+    : MAIN_ROUTE;
+  if (routerConfig[trimmed]) {
+    routerConfig[trimmed]();
+  } else {
+    window.location.hash = NOT_FOUND;
   }
 };
+
+export const initRouter = () => {
+  window.addEventListener("hashchange", router);
+  router();
+};
+
+function withLoginProtected(routeFunction) {
+  return (...args) => {
+    const userData = { name: "Alex" };
+    // select from storage
+    if (userData) {
+      routeFunction(userData, ...args);
+    } else {
+      window.location.hash = LOGIN_ROUTE;
+    }
+  };
+}
+
+function withPageWrapper(routeFunction) {
+  return (...args) => {
+    let wrapper = document.getElementById("main");
+
+    if (!wrapper) {
+      PAGE_REF.innerHTML = "";
+      const main = document.createElement("main");
+      main.innerHTML = `<header></header><div id="main"></div>`;
+      wrapper = main.querySelector("#main");
+      PAGE_REF.appendChild(main);
+    }
+
+    routeFunction(wrapper, ...args);
+  };
+}
